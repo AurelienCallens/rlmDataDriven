24a7240670744c46b095719d8f9df1ad150d84be
AurelienCallens <43750332+AurelienCallens@users.noreply.github.com>
2019-04-25 13:47:23 +0200

    Corrected function

--- R/rlmDD_het.R
+++ R/rlmDD_het.R
@@ -1,7 +1,6 @@
 rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"), 
-                       phi.par = TRUE, tuning.para = NULL, step = 0.1, n.lag = NULL, 
-                       print.summary = TRUE) 
-{
+                           phi.par = TRUE, tuning.para = NULL, step = 0.1, n.lag = NULL, 
+                           print.summary = TRUE){
   Y <- as.matrix(yy)
   n <- length(Y)
   z <- c()
@@ -9,10 +8,10 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
   X <- as.matrix(rlm.model$x)
   switch(var.function, power = {
     est <- function(x) {
-      sum(chi((Y - mu)/(phi * abs(mu)^x)) * log(abs(mu)))
+      sum(rlmDataDriven::chi((Y - mu)/(phi * abs(mu)^x)) * log(abs(mu)))
     }
     est_lag <- function(x) {
-      sum(chi((Y[-c(1:nlag)] - mu)/(phi * abs(mu)^x)) * 
+      sum(rlmDataDriven::chi((Y[-c(1:nlag)] - mu)/(phi * abs(mu)^x)) * 
             log(abs(mu)))
     }
     var.func <- function(mu, esti, phi) {
@@ -20,10 +19,10 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
     }
   }, exponential = {
     est <- function(x) {
-      sum(chi((Y - mu)/(phi * exp(x * abs(mu)))) * abs(mu))
+      sum(rlmDataDriven::chi((Y - mu)/(phi * exp(x * abs(mu)))) * abs(mu))
     }
     est_lag <- function(x) {
-      sum(chi((Y[-c(1:nlag)] - mu)/(phi * exp(x * abs(mu)))) * 
+      sum(rlmDataDriven::chi((Y[-c(1:nlag)] - mu)/(phi * exp(x * abs(mu)))) * 
             abs(mu))
     }
     var.func <- function(mu, esti, phi) {
@@ -56,7 +55,7 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
                                                     W * Y))
       mu <- X %*% B_est
       pearson_res <- (Y - mu)/(var.func(mu, esti, phi))
-      psip <- dpsi.Huber(pearson_res, c)
+      psip <- rlmDataDriven::dpsi.Huber(pearson_res, c)
       mn <- mean(psip)
       p <- as.numeric(length(B_est))
       K <- 1 + p/n * var(psip)/(mn^2)
@@ -69,8 +68,7 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
       }
     }
     c <- c_i[which.min(indic)]
-  }
-  else {
+  }else{
     c <- tuning.para
   }
   mu <- rlm.model$fitted.values
@@ -78,8 +76,7 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
   esti <- uniroot(est, c(-4, 4))$root
   if (phi.par == T) {
     phi <- median(abs(Y - mu)/(var.func(mu, esti, phi = 1)))/0.6745
-  }
-  else {
+  }else {
     phi <- 1
   }
   pearson_res <- (Y - mu)/(var.func(mu, esti, phi))
@@ -101,7 +98,9 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
   if (nlag == 0) {
     stop("Number of lag must be at least 1")
   }
+  
   list.l <- create_lag(pearson_res, n.lag = nlag)
+  
   for (a in 1:nlag) assign(paste0("lag", a), list.l[[a]])
   dat.l <- as.data.frame(list.l) * (var.func(mu, esti, phi))
   dat.ln <- matrix(c(rep(1, nlag)), ncol = 1)
@@ -112,23 +111,10 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
                              X[-c(1:nlag), ]), crossprod(X[-c(1:nlag), ], W[-c(1:nlag)] * 
                                                            Y[-c(1:nlag)]))
   mu <- X[-c(1:nlag), ] %*% B_est
-  esti <- uniroot(est_lag, c(-4, 4))$root
-  if (phi.par == T) {
-    phi <- median(abs(Y[-c(1:nlag)] - mu)/(var.func(mu, 
-                                                    esti, phi = 1)))/0.6745
-  }
-  else {
-    phi <- 1
-  }
+
   pearson_res <- (Y[-c(1:nlag)] - mu)/(var.func(mu, esti, 
                                                 phi))
-  w_i <- as.vector((psi.Huber(pearson_res, c)/(pearson_res)))
-  W <- pmin(as.vector(w_i/(var.func(mu, esti, phi)))^2, 1)
-  B_est <- solve(crossprod(X[-c(1:nlag), ], W * X[-c(1:nlag), 
-                                                  ]), crossprod(X[-c(1:nlag), ], W * Y[-c(1:nlag)]))
-  mu <- X[-c(1:nlag), ] %*% B_est
-  pearson_res <- (Y[-c(1:nlag)] - mu)/(var.func(mu, esti, 
-                                                phi))
+
   p <- length(B_est)
   psip <- dpsi.Huber(pearson_res, c)
   mn <- mean(psip)
@@ -147,14 +133,14 @@ rlmDD_het <- function (yy, xx, var.function = c("power", "exponential"),
   z$summary <- sum_table
   z$model <- cbind(yy, X)
   z$p_residuals <- pearson_res
-  z$r_residuals <- psi.Huber(pearson_res, c = c)
+  z$r_residuals <- psi.Huber((Y[-c(1:nlag)] - mu), c = c)
   z$tuningpara <- if (is.null(tuning.para)) {
     list(optimal = c, tested_values = c_i, obtained_var = indic)
   }
   else {
     list(c = c)
   }
-  z$varpara <- list(var_func = var.function, gamma = esti, phi = phi)
+  z$varpara <- data.frame(gamma = esti, phi = phi)
   if (print.summary) {
     cat("Summary : \n ")
     print(z$summary)
